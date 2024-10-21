99e1e962a881de58c3f2bd1aba85e5e234e63b0a
Skrebnev Fedor <40490810+Skrebnevf@users.noreply.github.com>
2024-10-21 20:54:36 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github/skrebnevf/comedy_belgrade_bot/pkg/handlers"
 	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
+
 	config, err := LoadConfig()
 	if err != nil {
 		log.Fatal(err)
@@ -15,6 +19,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("cannot init bot, error: %v", err)
 	}
+
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Bot is running")
+		})
+
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8000"
+		}
+
+		log.Printf("Listening on port %s for health checks...", port)
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	handlers.CommandHandlers(b)
 	handlers.TextHandler(b)
