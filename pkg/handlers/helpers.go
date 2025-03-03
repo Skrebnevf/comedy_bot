82e299730a0e6371d531012d79cd0b2014971adb
f.skrebnev <f.skrebnev@getic.com>
2025-03-03 21:28:13 +0100

    add user agent

--- pkg/handlers/helpers.go
+++ pkg/handlers/helpers.go
@@ -7,14 +7,26 @@ import (
 )
 
 func WakeUp(url string, command string) {
-	resp, err := http.Get(url)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Printf("bot is not wake up: %v", err)
+		log.Printf("cannot create request: %v", err)
 	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("cannot client request: %v", err)
+	}
+	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf("cannot read body from wake up: %v", err)
+		log.Printf("cannot get body request: %v", err)
 	}
 
 	log.Println(string(body) + command)
