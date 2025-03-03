2bac92ede89664fe0203d00c4688cdf3a44b121f
f.skrebnev <f.skrebnev@getic.com>
2025-03-03 20:04:08 +0100

    change error log

--- pkg/handlers/helpers.go
+++ pkg/handlers/helpers.go
@@ -9,12 +9,12 @@ import (
 func WakeUp(url string, command string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("bot is not wake up: %v", err)
+		log.Printf("bot is not wake up: %v", err)
 	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("cannot read body from wake up: %v", err)
+		log.Printf("cannot read body from wake up: %v", err)
 	}
 
 	log.Println(string(body) + command)
